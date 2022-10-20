b5514213c86adb8a77969f32af7062f69c1e8a09
randy <randyr_1993@yahoo.com>
2022-10-20 01:20:42 -0400

    Commiting comments

--- src/main/java/com/reicode/crudpractice/PersonRepository.java
+++ src/main/java/com/reicode/crudpractice/PersonRepository.java
@@ -3,7 +3,12 @@ package com.reicode.crudpractice;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-//This is the class that allows us to speak to the database
+
+/** This is the class that allows us to speak to the database
+ * By extending CrudRepository, PersonRepository inherits several methods for working with "Person" persistence.
+ * Including: saving, deleting, finding "Person" entities.
+ */
+
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
 }
